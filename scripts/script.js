6da6c5d1fe66b88fe6c9f8756aeb27d76200d450
hasan960 <mahmudulhasan99988@gmail.com>
2024-02-20 09:46:00 +0600

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+
+const allBtn = document.getElementsByClassName("add-btn");
+
+let count = 40 ;
+let selectedSeat = 0 ;
+
+for (const btn of allBtn) {
+    btn.addEventListener('click',function () {
+        count = count -1
+        document.getElementById("total-seat").innerText = count;
+    });
+    btn.addEventListener('click',function () {
+        selectedSeat = selectedSeat + 1 ;
+        document.getElementById("selectedSeat").innerText = selectedSeat;
+    });
+    btn.addEventListener('click',function () {
+        btn.classList.add("bg-green-700","text-white");
+    });
+
+}
+
+
